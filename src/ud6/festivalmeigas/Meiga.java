920ecf1ac1506b1d1ccad156d56ab4a2374d9f86
aguadodev <aguado@edu.xunta.es>
2025-04-25 12:54:06 +0200

    Clase Meiga

--- src/ud6/festivalmeigas/Meiga.java
+++ src/ud6/festivalmeigas/Meiga.java
@@ -1,5 +1,49 @@
 package ud6.festivalmeigas;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+
 public class Meiga {
+    // Atributos
+
+
+    // Constructores
+
+
+
+    // Métodos
+
+
+
+    public static Meiga[] crearMeigasExemplo(Feitizo[] feitizosDisponibles) {
+        Random rnd = new Random();
+    
+        String[] nomes = {"Uxía", "Lúa", "Iria", "Noa"};
+        String[] alcumes = {"A das Fraguiñas", "Sombra do Vento", "Luz do Bosque", "Meiga da Brétema"};
+    
+        Meiga[] meigas = new Meiga[nomes.length];
+
+        // Obtén todos os feitozos de exemplo e todos os seus ingredientes únicos
+        Collection<Feitizo> feitizosExemplo = List.of(Feitizo.crearFeitizosExemplo());
+        Collection<String> ingredientesExemplo = Feitizo.ingredientesUnicos(feitizosExemplo);
+
+        for (int i = 0; i < nomes.length; i++) {
+            String nome = nomes[i];
+            String alcume = alcumes[i];
+            // Xera unha colección cunha selección aleatoria de feitizos de exemplo para asignar á meiga
+            Collection<Feitizo> feitizos = getFeitizosRnd(feitizosExemplo);
+            // Xera un mapa cunha selección aleatoria de ingredientes e cantidades de cada un de exemplo para asignar á meiga
+            Map<String, Integer> inventario = getIngredientesRnd(ingredientesExemplo, 3);
+    
+            // Crea a meiga e a engade ao array
+            meigas[i] = new Meiga(nome, alcume, feitizos, inventario);
+        }
+
+        return meigas;
+
+    }
 
 }
