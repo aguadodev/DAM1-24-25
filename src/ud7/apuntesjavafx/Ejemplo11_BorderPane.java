4c7ad367802e2eec19fabe7cfb62c9b8696888e7
aguadodev <aguado@edu.xunta.es>
2025-05-09 14:38:01 +0200

    fxml

--- src/ud7/apuntesjavafx/Ejemplo11_BorderPane.java
+++ src/ud7/apuntesjavafx/Ejemplo11_BorderPane.java
@@ -1,9 +1,17 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
-public class Ejemplo11_BorderPane extends Application{
+public class Ejemplo11_BorderPane extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -11,14 +19,29 @@ public class Ejemplo11_BorderPane extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        MenuItem menRojo = new MenuItem("Rojo");
+        MenuItem menVerde = new MenuItem("Verde");
+        MenuItem menAzul = new MenuItem("Azul");
 
-        BorderPane borderPane = new BorderPane(null);
+        Menu menColores = new Menu("Colores");
+        menColores.getItems().addAll(menRojo, menVerde, menAzul);
 
-        Scene scene = new Scene(borderPane, 300, 200);
+        MenuBar menuBar = new MenuBar(menColores);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(menuBar);
+        
+        // EVENTOS
+        menRojo.setOnAction(e -> borderPane.setBackground(Background.fill(Paint.valueOf("RED"))));
+        menVerde.setOnAction(e -> borderPane.setStyle("-fx-background-color: green;"));
+        menAzul.setOnAction(e -> borderPane.setStyle("-fx-background-color: blue;"));
+
+
+        Scene scene = new Scene(borderPane, 400, 200);
         primaryStage.setScene(scene);
         primaryStage.setTitle("BorderPane con Menú y Colores de fondo");
         primaryStage.setResizable(false);
         primaryStage.show();
     }
-    
+
 }
