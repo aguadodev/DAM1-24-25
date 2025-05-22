1c2b972a0ea5aa338c4a6a95d306def53b2c6e5f
aguadodev <aguado@edu.xunta.es>
2025-05-22 12:52:02 +0200

    Exame UD& - REC Borrador

---
+++ src/ud6/xxxexamenrec/xxxexamenrec/musicollab/Musico.java
@@ -0,0 +1,18 @@
+package ud6.xxxexamenrec.xxxexamenrec.musicollab;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class Musico {
+
+    public Musico(String string, String string2, List<String> of, HashSet hashSet) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Collection<? extends String> getInstrumentos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getInstrumentos'");
+    }
+
+}
