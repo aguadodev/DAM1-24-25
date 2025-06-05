fcf7d1fc5bf2b93abc9a596dc891987fbdda0baa
aguadodev <aguado@edu.xunta.es>
2025-06-05 12:47:47 +0200

    Ajustes

--- src/ud5/praiasdegalicia/AppBanderaAzul.java
+++ src/ud5/praiasdegalicia/AppBanderaAzul.java
@@ -3,7 +3,7 @@ package ud5.praiasdegalicia;
 public class AppBanderaAzul {
     public static void main(String[] args) {
         // Carga las playas desde el fichero JSON en un array de Praias
-        Praia[] praias = Util.importarPraias("praias.json");
+        Praia[] praias = Util.importarPraias("datos/praias.json");
 
         // TODO Resuelve el problema propuesto
         // ...
--- src/ud5/praiasdegalicia/AppClase.java
+++ src/ud5/praiasdegalicia/AppClase.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class AppClase {
     public static void main(String[] args) {
         // Carga las playas desde el fichero JSON en un array de Praias
-        Praia[] praias = Util.importarPraias("praias.json");
+        Praia[] praias = Util.importarPraias("datos/praias.json");
 
         // Ordenar por método natural (id)
         Arrays.sort(praias);
